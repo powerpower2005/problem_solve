bfc04e1dd9327cb6d894137eb00058993bf041ac
Seeunggyu Park <57701607+powerpower2005@users.noreply.github.com>
2022-09-26 13:47:12 +0900

    [Gold V] Title: ABCDE, Time: 1928 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> friends[2001];
+bool visit[2001];
+bool isPossible = false;
+
+
+void DFS(int node, int depth){
+    visit[node] = true;
+    if(depth >= 5){
+        isPossible = true;
+        visit[node] = false;
+        return;
+    }
+    int size = friends[node].size();
+    for(int i = 0; i<size; i++){
+        int nextNode = friends[node][i];
+        if(visit[nextNode] == false){
+            DFS(nextNode, depth + 1);
+        }
+    }
+    visit[node] = false;
+}
+
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    for(int i = 0; i<m; i++){
+        int u,v;
+        cin >> u >> v;
+        friends[u].push_back(v);
+        friends[v].push_back(u);
+    }
+    
+    for(int i = 0; i<m; i++){
+        DFS(i,1);
+    }
+    
+    if(isPossible){
+        cout << "1\n";
+    }else{
+        cout << "0\n";
+    }
+    
+    
+    
+    
+    return 0;
+}
